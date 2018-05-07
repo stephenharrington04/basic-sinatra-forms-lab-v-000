29f0818650fbbf6b1c502ee63419930b0f9e5eb3
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-07 21:17:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
 
     erb :team
   end
